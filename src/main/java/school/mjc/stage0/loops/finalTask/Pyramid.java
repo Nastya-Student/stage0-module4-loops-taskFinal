e8a23f9a532d6c784785ce3e11ec838e580082ac
Nastya Belova <nastjabelova0@gmail.com>
2024-06-28 00:36:13 +0300

    done

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,19 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        for (int i = 0; i < cathetusLength; i++) {
+//            System.out.println(i);
+            for(int j = 0; j < cathetusLength - i - 1; j ++){
+                System.out.print(" ");
+            }
+            for (int j = i + 1; j > 0 ; j--) {
+                System.out.print(j);
+            }
+            for (int j = 2; j <= i + 1; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
